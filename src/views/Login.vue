6df30250a474b2f6201f0a7bf22f2fede4e64b90
eugeni blas <eugeniblas@yahoo.com>
2022-05-08 09:51:04 +0200

    Login-Register v1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,70 +12,111 @@
         </div>
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
-                <div class="col-lg-5">
+                <div class="col-lg-6">
                     <card type="secondary" shadow
                           header-classes="bg-white pb-5"
                           body-classes="px-lg-5 py-lg-5"
                           class="border-0">
                         <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
-                            </div>
-                        </template>
-                        <template>
                             <div class="text-center text-muted mb-4">
-                                <small>Or sign in with credentials</small>
+                              <small>LOGIN</small>
                             </div>
                             <form role="form">
-                                <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Email"
-                                            addon-left-icon="ni ni-email-83">
-                                </base-input>
-                                <base-input alternative
-                                            type="password"
-                                            placeholder="Password"
-                                            addon-left-icon="ni ni-lock-circle-open">
-                                </base-input>
-                                <base-checkbox>
-                                    Remember me
-                                </base-checkbox>
-                                <div class="text-center">
-                                    <base-button type="primary" class="my-4">Sign In</base-button>
-                                </div>
+                              <label>Email</label>
+                              <base-input alternative
+                                          class="mb-3"
+                                          placeholder="Email"
+                                          v-model="user">
+                              </base-input>
+                              <label>Password</label>
+                              <base-input alternative
+                                          :type="typePassword"
+                                          placeholder="Password"
+                                          v-model="password">
+                              </base-input>
+                              <button v-on:click="showPassword()">
+                                <small>{{ actionPassword }}</small>
+                              </button>
+                              <base-checkbox>
+                                Remember me
+                              </base-checkbox>
+                              <div class="text-center">
+                                <base-button type="primary" class="my-4" v-on:click="validateUserPsw()">
+                                  LOGIN
+                                </base-button>
+                                <br>
+                                <a href="#" class="text-light">
+                                  <small>Forget your password?</small>
+                                </a>
+                              </div>
                             </form>
                         </template>
+                        <template>
+                            <hr class="hr-black">
+                            <div class="text-muted text-center mb-3">
+                                <small>ALTERNATIVE LOGIN</small>
+                            </div>
+                            <div class="btn-wrapper text-center">
+                                <base-button type="neutral" v-on:click="showPassword()">
+                                    <img slot="icon" src="img/icons/common/github.svg">
+                                    Login Github
+                                </base-button>
+                                <p></p>
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/google.svg">
+                                    Login Google
+                                </base-button>
+                            </div>
+                        </template>
                     </card>
-                    <div class="row mt-3">
-                        <div class="col-6">
-                            <a href="#" class="text-light">
-                                <small>Forgot password?</small>
-                            </a>
-                        </div>
-                        <div class="col-6 text-right">
-                            <a href="#" class="text-light">
-                                <small>Create new account</small>
-                            </a>
-                        </div>
+                    <div class="btn-wrapper text-center">
+                        <base-button type="primary" class="my-4">
+                          CREATE NEW ACCOUNT
+                        </base-button>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: "",
+      password: "",
+      typePassword: "password",
+      actionPassword: "Show password",
+    };
+  },
+  methods: {
+    showPassword: function() {
+      if (this.typePassword === "password") {
+        this.typePassword = "text";
+        this.actionPassword = "Hide password";
+      }  else {
+        this.typePassword = "password";
+        this.actionPassword = "Show password";
+      }
+    },
+    validateUserPsw: function() {
+      if (this.user !==  "aitana2022@gmail.com") {
+        window.alert("wrong user");
+      }else {
+        if (this.password !==  "2022") {
+          window.alert("password incorrecta");
+        } else {
+          window.alert("Welcome Aitana");
+        }
+      };
+    },
+  },
+};
 </script>
+
 <style>
+.hr-black {
+  border-top: 1px solid black;
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,70 +12,110 @@
         </div>
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
-                <div class="col-lg-5">
+                <div class="col-lg-6">
                     <card type="secondary" shadow
                           header-classes="bg-white pb-5"
                           body-classes="px-lg-5 py-lg-5"
                           class="border-0">
-                        <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
-                            </div>
-                        </template>
-                        <template>
-                            <div class="text-center text-muted mb-4">
-                                <small>Or sign up with credentials</small>
-                            </div>
-                            <form role="form">
-                                <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Name"
-                                            addon-left-icon="ni ni-hat-3">
-                                </base-input>
-                                <base-input alternative
-                                            class="mb-3"
-                                            placeholder="Email"
-                                            addon-left-icon="ni ni-email-83">
-                                </base-input>
-                                <base-input alternative
-                                            type="password"
-                                            placeholder="Password"
-                                            addon-left-icon="ni ni-lock-circle-open">
-                                </base-input>
-                                <div class="text-muted font-italic">
-                                    <small>password strength:
-                                        <span class="text-success font-weight-700">strong</span>
-                                    </small>
-                                </div>
-                                <base-checkbox>
-                                    <span>I agree with the
-                                        <a href="#">Privacy Policy</a>
-                                    </span>
-                                </base-checkbox>
-                                <div class="text-center">
-                                    <base-button type="primary" class="my-4">Create account</base-button>
-                                </div>
-                            </form>
-                        </template>
+                      <template>
+                          <div class="text-center text-muted mb-4">
+                              <small>SIGN UP</small>
+                          </div>
+                          <form role="form" id="signUpForm" >
+                              <label>Email</label>
+                              <base-input alternative
+                                          class="mb-3"
+                                          placeholder="Email"
+                                          v-model="user">
+                              </base-input>
+                              <label>Password</label>
+                              <base-input alternative
+                                          :type="typePassword"
+                                          placeholder="Password"
+                                          v-model="password">
+                              </base-input>
+                              <label>First Name</label>
+                              <base-input alternative
+                                          class="mb-3"
+                                          placeholder="First Name"
+                                          v-model="firstName">
+                              </base-input>
+                              <label>Last Name</label>
+                              <base-input alternative
+                                          class="mb-3"
+                                          placeholder="Last Name"
+                                          v-model="lastName">
+                              </base-input>
+                              <base-checkbox>
+                                  <span>I agree with
+                                      <a href="#">Privacy Policy</a>
+                                  </span>
+                              </base-checkbox>
+                              <div class="text-center">
+                                  <base-button type="primary" class="my-4">SIGN UP</base-button>
+                              </div>
+                          </form>
+                      </template>
+                      <template>
+                        <hr class="hr-black">
+                        <div class="text-muted text-center mb-3">
+                          <small>ALTERNATE SIGNUP</small>
+                        </div>
+                        <div class="btn-wrapper text-center">
+                          <base-button type="neutral" v-on:click="showPassword()">
+                            <img slot="icon" src="img/icons/common/github.svg">
+                            Signup Github
+                          </base-button>
+                          <p></p>
+                          <base-button type="neutral">
+                            <img slot="icon" src="img/icons/common/google.svg">
+                            Signup Google
+                          </base-button>
+                        </div>
+                      </template>
                     </card>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: "",
+      password: "",
+      typePassword: "password",
+      firstName: "",
+      lastName: "",
+    };
+  },
+  methods: {
+    showPassword: function() {
+      if (this.typePassword === "password") {
+        this.typePassword = "text";
+      }  else {
+        this.typePassword = "password";
+      }
+    },
+    validateUserPsw: function() {
+      if (this.user !==  "aitana2022@gmail.com") {
+        window.alert("Usuario inexistente");
+      }else {
+        if (this.password !==  "2022") {
+          window.alert("password incorrecta");
+        } else {
+          window.alert("Bienvenida Aitana");
+        }
+      };
+    },
+  },
+};
 </script>
+
 <style>
+.hr-black {
+  border-top: 1px solid black;
+}
 </style>
