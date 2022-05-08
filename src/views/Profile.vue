acd0a6884180b4e94c03c94bca10e55892f95e15
lisa31419 <isa31419@gmail.com>
2022-05-09 00:21:30 +0200

    In progress parts

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -34,16 +34,18 @@
               href="#button"
               class="btn btn-default rounded-circle btn-lg"
               rel="tooltip"
-              title="Follow me on Twitter"
+              title="Update Profile"
+              @click="inProgress()"
           >
             <i class="fa fa-refresh" aria-hidden="true"></i>
-            Modificar perfil
+            Update profile
           </a>
           <a
               href="#button"
               class="btn btn-default rounded-circle btn-lg"
               rel="tooltip"
-              title="Follow me on Instagram"
+              title="CV"
+              @click="inProgress()"
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
               <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z"/>
@@ -424,7 +426,9 @@ export default {
     }
   },
   methods: {
-
+    inProgress() {
+      this.$swal('Warning', 'This feature is still in construction\n Please, try other features.', 'warning')
+    }
   }
 };
 </script>
