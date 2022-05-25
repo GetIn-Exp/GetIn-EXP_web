f1f69f6539ea23e2e57f6c073b46367936e3e4f3
spurnik <arnau6martinez@gmail.com>
2022-05-25 19:30:34 +0200

    Basic modifications (Faltan feedback messages)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -79,8 +79,8 @@
                                   class="form-control dropdown-input border-right-0"
                                   v-model="what"
                                   :class="[
-                                    //{ 'border-primary': userIsSearching && whatIsValid },
-                                    //{ 'border-danger': userIsSearching && !whatIsValid },
+                                    //{ 'border-primary': (!userIsSearching) && whatIsValid },
+                                    //{ 'border-danger': (!userIsSearching) && !whatIsValid },
                                   ]"
                                   @click="userIsSearching = true; what = ''; "
                                 />
@@ -242,6 +242,7 @@
           <div class="col-lg-8 mt-3">
             
             <!-- DISPLAY OFFERTS -->
+            
             <div v-if="lastSearchValid" id="home-results">
               <div class="mb-3 ml-4 mr-4">
                 <h4>
@@ -282,7 +283,22 @@
               </ul>
             </div>
 
+            <!-- FEEDBACK Messages
+            <div v-else-if="firstTimeSearch" id="home-error-results">
+              <div class = "row justify-content text-center">
+                <h4> Oooh... parece que no se han encontrado resultados para tus parámetros de búsqueda.</h4>
+              </div>
+            </div>
+
+            <div v-else id="home-no-results">
+              <div class = "row justify-content text-center">
+                <h4> Introduce los parámetros de búsqueda.</h4>
+              </div>
+            </div>
+            -->
+
           </div>
+
         </div>
       </div>
   </div>
@@ -333,6 +349,7 @@ export default {
       lastResultList: [],
 
       userIsSearching: false,
+      firstTimeSearch: false,
 
       // TODO: json request from url route
       listaOfertas: [
@@ -1071,6 +1088,7 @@ export default {
       this.lastResultList = this.listaOfertasFiltrada;
       this.lastSearchValid = this.newSearchValid;
       this.userIsSearching = false;
+      this.firstTimeSearch = true;
     },
 
     selectWhat(event){
